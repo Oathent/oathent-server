d1b0fffa3de3aa3dfb22237db1872d7f3cda6b54
Barnaby <22575741+barnabwhy@users.noreply.github.com>
2023-07-26 22:20:38 +0100

    Rename to a better name

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getIndex(): string {
-    return `VersAuth v${process.env.npm_package_version || '0.0.0'}`;
+    return `Oathent v${process.env.npm_package_version || '0.0.0'}`;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -6,34 +6,36 @@ import { ValidationPipe } from '@nestjs/common';
 import { SCOPES, initialiseScopes } from './auth/scopes';
 
 configDotenv({
-  path: 'postgres.env'
+    path: 'postgres.env'
 });
 configDotenv();
 
 async function bootstrap() {
-  if(!process.env.DATABASE_URL)
-    throw new Error("DATABASE_URL was not defined in .env!")
+    if (!process.env.DATABASE_URL)
+        throw new Error("DATABASE_URL was not defined in .env!")
 
-  await initialiseScopes();
+    await initialiseScopes();
 
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  app.useGlobalPipes(new ValidationPipe());
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    app.useGlobalPipes(new ValidationPipe());
 
-  const docsConfig = new DocumentBuilder()
-    .setTitle('VersAuth API')
-    .addBearerAuth({ type: 'http' }, 'Account access token')
-    .addBearerAuth({ type: 'http' }, 'Account refresh token')
-    .addBearerAuth({ type: 'http' }, 'OAuth2 access token')
-    .addBearerAuth({ type: 'http' }, 'OAuth2 refresh token')
-    .addBearerAuth({ type: 'http' }, 'OAuth2 auth code')
-    .addBearerAuth({ type: 'http' }, 'OAuth2 device code')
-    .setVersion(process.env.npm_package_version || '0.0.0')
-    .setDescription(`Authentication and OAuth2 API<br><br>**Scopes:** ${Object.keys(SCOPES).join(', ')}`)
-    .build();
-  const document = SwaggerModule.createDocument(app, docsConfig);
-  SwaggerModule.setup('docs', app, document);
+    if (!process.env.DISABLE_SWAGGER || process.env.DISABLE_SWAGGER.toLowerCase() != "yes") {
+        const docsConfig = new DocumentBuilder()
+            .setTitle('Oathent API')
+            .addBearerAuth({ type: 'http' }, 'Account access token')
+            .addBearerAuth({ type: 'http' }, 'Account refresh token')
+            .addBearerAuth({ type: 'http' }, 'OAuth2 access token')
+            .addBearerAuth({ type: 'http' }, 'OAuth2 refresh token')
+            .addBearerAuth({ type: 'http' }, 'OAuth2 auth code')
+            .addBearerAuth({ type: 'http' }, 'OAuth2 device code')
+            .setVersion(process.env.npm_package_version || '0.0.0')
+            .setDescription(`Authentication and OAuth2 API<br><br>**Scopes:** ${Object.keys(SCOPES).join(', ')}`)
+            .build();
+        const document = SwaggerModule.createDocument(app, docsConfig);
+        SwaggerModule.setup('docs', app, document);
+    }
 
-  await app.listen(3000);
+    await app.listen(3000);
 }
 bootstrap();
