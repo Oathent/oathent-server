8f871fdd7bfdf78e807ef7addb890d88fc112d7e
Barnaby <22575741+barnabwhy@users.noreply.github.com>
2023-07-26 22:13:30 +0100

    move expiry times to env

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -2,13 +2,13 @@ import { randomBytes } from "crypto";
 
 export const jwtConstants = {
     accessSecret: Buffer.from(process.env.JWT_ACCESS_SECRET, 'base64') || randomBytes(256).toString('base64'),
-    accessExpiry: '6h',
+    accessExpiry: process.env.JWT_ACCESS_EXPIRY || '6h',
 
     refreshSecret: Buffer.from(process.env.JWT_REFRESH_SECRET, 'base64') || randomBytes(256).toString('base64'),
-    refreshExpiry: '30d',
+    refreshExpiry: process.env.JWT_REFRESH_EXPIRY || '30d',
 
-    authCodeExpiry: '30s',
+    authCodeExpiry: process.env.JWT_AUTH_CODE_EXPIRY || '30s',
 
-    deviceCodeSecret: Buffer.from(process.env.JWT_DEVICE_SECRET, 'base64') || randomBytes(256).toString('base64'),
-    deviceCodeExpiry: '5m',
+    deviceCodeSecret: Buffer.from(process.env.JWT_DEVICE_CODE_SECRET, 'base64') || randomBytes(256).toString('base64'),
+    deviceCodeExpiry: process.env.JWT_DEVICE_CODE_EXPIRY || '5m',
 };
